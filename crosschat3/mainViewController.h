a1db83635c9e95eaa0f5b83bf82f7d1685dea855
Darshan Aralakuppe Prakash <apdarshan@gmail.com>
2013-04-28 23:53:58 +0530

    Working Chat , without history display

--- crosschat3/mainViewController.h
+++ crosschat3/mainViewController.h
@@ -7,9 +7,20 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "SocketIO.h"
+//#import "SRWebSocket.h"
+#import "SocketIOPacket.h"
 
-@interface mainViewController : UIViewController
+@interface mainViewController : UIViewController<SocketIODelegate>
+
 @property (weak, nonatomic) IBOutlet UILabel *welcomeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *fromUserLabel;
+@property (weak, nonatomic) IBOutlet UITextView *incomingMsgTextView;
+@property (weak, nonatomic) IBOutlet UITextField *sendingMsgTextFeild;
+
 - (IBAction)logoutAction:(id)sender;
+- (IBAction)textFieldDoneEditing:(id)sender;
+- (IBAction)sendMessageAction:(id)sender;
+- (void)updateIncomingMsgSection:(NSDictionary *)args;
 
 @end
--- crosschat3/mainViewController.m
+++ crosschat3/mainViewController.m
@@ -10,10 +10,14 @@
 #import "viewController.h"
 
 @interface mainViewController ()
-
+{
+    SocketIO* socketIO;
+//    SRWebSocket* webSocketIO;
+}
 @end
 
 @implementation mainViewController
+
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -29,6 +33,12 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     self.welcomeLabel.text = [[NSString alloc]initWithFormat:@"Welcome %@,",[ViewController username]];
+    
+    
+    socketIO = [[SocketIO alloc]initWithDelegate:self];
+    [socketIO connectToHost:@"localhost" onPort:80];
+    
+        
 }
 
 - (void)didReceiveMemoryWarning
@@ -37,10 +47,60 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+
+-(void)socketIO:(SocketIO *)socket didReceiveEvent:(SocketIOPacket *)packet{
+    
+    
+    NSLog(@"Event triggered packet: data:%@ type:%@", packet.data, packet.type);
+    NSDictionary* response = [NSDictionary dictionaryWithDictionary:packet.dataAsJSON];
+
+   if ([[response objectForKey:@"name"] isEqualToString:@"messagein"]) {
+       
+//        if ([[response objectForKey:@"name"] isEqualToString:@"task"]&&[response objectForKey:@"args"]&&[[response objectForKey:@"args"] count]>0) {
+//            self.incomingMsgTextView.text = [self.incomingMsgTextView.text stringByAppendingFormat:@"\n%@",[[response objectForKey:@"args"] objectAtIndex:0]];
+//        }
+       
+       self.incomingMsgTextView.text = [[[response objectForKey:@"args"] objectAtIndex:0] objectForKey:@"msg"];
+       self.fromUserLabel.text = [[[response objectForKey:@"args"] objectAtIndex:0] objectForKey:@"username"];
+       self.fromUserLabel.text = [self.fromUserLabel.text stringByAppendingFormat:@"%@", @": "];
+   }
+
+}
+
+
+- (void)updateIncomingMsgSection:(NSDictionary *)args {
+    self.fromUserLabel.text = [args objectForKey:@"username"];
+    self.incomingMsgTextView.text  = [args objectForKey:@"msg"];
+}
+
+
+- (void) socketIO:(SocketIO *)socket failedToConnectWithError:(NSError *)error
+{
+    NSLog(@"failedToConnectWithError() %@", error);
+}
+
+
 - (IBAction)logoutAction:(id)sender {
     [ViewController setUsername:nil];
     ViewController* vc = [[ViewController alloc]init];
     //[self parentViewController];
     [self presentViewController:vc animated:YES completion:nil];
 }
+
+- (IBAction)textFieldDoneEditing:(id)sender {
+    [sender resignFirstResponder];
+}
+
+- (IBAction)sendMessageAction:(id)sender {
+    
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    [dict setObject:self.sendingMsgTextFeild.text forKey:@"message"];
+    [dict setObject:[ViewController username] forKey:@"username"];
+    
+    [socketIO sendEvent:@"messagesend" withData:dict];
+    self.sendingMsgTextFeild.text = @"";
+    
+}
+
 @end
